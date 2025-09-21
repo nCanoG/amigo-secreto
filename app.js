47dd48d80cd5122f1a6b85de5c229c2baaf73cbd
nCanoG <nicocano1998@gmail.com>
2025-09-21 16:50:12 -0300

    Se actualiza app.js
    Se carga archivo de challenge trabajado y funcionando, listo para la entrega.

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables
+let amigos =[]
+
+//funciones
+function agregarAmigo() {
+    let listaResultados = document.getElementById('resultado');
+    listaResultados.innerHTML = '';
+    let inputNombre = document.getElementById('amigo');
+    let nombre = inputNombre.value;
+    if (nombre.trim() === ''){
+        alert ('Favor ingresar un nombre válido');
+        return;
+    }
+    amigos.push(nombre);
+    actualizarListaAmigos ();
+    inputNombre.value = '';
+    }
+
+function actualizarListaAmigos() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = amigos[i];
+        listaHTML.appendChild(item);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert('¡Debes agregar al menos dos amigo para sortear!');
+        return; 
+    }
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let ganador = amigos[indiceAleatorio];
+    let listaResultados = document.getElementById('resultado');
+    listaResultados.innerHTML = `<li>¡El amigo secreto es: ${ganador}!</li>`;
+    amigos = [];
+    actualizarListaAmigos();
+}
+
